dca3ef229d288a7872755d4fd146cd824396bac0
sagi <misan.sagi@gmail.com>
2022-04-04 22:32:01 +0300

    shopping list

--- db.ts
+++ db.ts
@@ -2,23 +2,43 @@ const { MongoClient } = require("mongodb");
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 // Database Name
-const dbName = 'testdb';
+const dbName = 'shoppingLists';
 
 export default class db{
-   public static async main(result:any) {
+    public static async main(result:string) {
     // Use connect method to connect to the server
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
-    const collection = db.collection('products');
+    const collection = db.collection('lists');
     collection.insertOne({result})
     console.log('collection.find()',result);
     // the following code examples can be pasted here...
     return 'done.';
+    
+    // // Use connect method to connect to the server
+    // await client.connect();
+    // console.log('Connected successfully to server');
+    // const db = client.db(dbName);
+    // const collection = db.collection('products');
+    // collection.insertOne({result})
+    // console.log('collection.find()',result);
+    // // the following code examples can be pasted here...
+    // return 'done.';
   }
+
+  public static async getShoppingList() {
+    // Use connect method to connect to the server
+    await client.connect();
+    const db = client.db(dbName);
+    db.collection('lists').findOne({},(err:any,result:any)=>{
+      if (err) throw err;
+      console.log('res',result);
+      return result
+    });
 }
 
-
+}
 // main()
 //   .then(console.log)
 //   .catch(console.error)
--- index.ts
+++ index.ts
@@ -1,15 +1,38 @@
+import db from "./db";
 
 const express = require("express");
-const db = require("./db");
 const SingleProScrup = require("./SingleProScrup");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = 3000;
-
+const { MongoClient } = require("mongodb");
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 // app
 io.on("connection", (socket: any) => {
   console.log("a user connected :D");
+  socket.on("updateShopingList", async (msg: string) => {
+    console.log(msg);
+    db.main(msg)
+    // db.main('')
+    // SingleProScrup.proNames(msg).then((result: any) => {
+    //   console.log('pro', result);
+    //   db.main(result)
+    //   io.emit("getProduct", result);
+    // })
+  });
+  socket.on("getShoppingList", async (msg: string) => {
+    // console.log(msg);
+    await client.connect();
+    const db = client.db('shoppingLists');
+    db.collection('lists').findOne({},(err:any,result:any)=>{
+      if (err) throw err;
+      console.log('res',result);
+          io.emit("getShoppingList",result);
+      // return result
+    });
+  })
   socket.on("scrap", async (msg: string) => {
     SingleProScrup.proNames(msg).then((result: any) => {
       console.log('pro', result);

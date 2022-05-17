1e66fcd71c1297f8f7793287a8db336748d81797
sagi <misan.sagi@gmail.com>
2022-05-17 20:41:52 +0300

    port

--- Globals.ts
+++ Globals.ts
@@ -1,4 +1,4 @@
 export class Globals{
     public static release=true
-    public static URL=Globals.release?'mongodb+srv://sagi:CNa%23ESi26Nbx%24tU@cluster0.tv3hh.mongodb.net/test':'mongodb://localhost:27017'
+    public static URL=Globals.release?'mongodb+srv://sagi:CNa%23ESi26Nbx%24tU@cluster0.tv3hh.mongodb.net/test?directConnection=true':'mongodb://localhost:27017'
 }
--- db.ts
+++ db.ts
@@ -6,7 +6,7 @@ const { MongoClient } = require("mongodb");
 const url = Globals.URL;
 const client = new MongoClient(url);
 // Database Name
-const dbName = 'superimDB';
+const dbName = 'userData';
 
 export default class db {
   public static async main(result: string) {

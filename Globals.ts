3ffeeb565e95000fc80368b05b51d8bc8c2b1962
sagi <misan.sagi@gmail.com>
2022-05-17 20:10:41 +0300

    port

--- Globals.ts
+++ Globals.ts
@@ -1,5 +1,4 @@
 export class Globals{
     public static release=true
     public static URL=Globals.release?'mongodb+srv://sagi:CNa%23ESi26Nbx%24tU@cluster0.tv3hh.mongodb.net/test':'mongodb://localhost:27017'
-    
 }
